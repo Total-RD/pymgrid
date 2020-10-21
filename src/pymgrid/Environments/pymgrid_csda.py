2e60a50b0f8d049141f9d6d09c28dd502a8358a2
Gonzague Henri <gonzague.henri@total.com>
2020-10-21 07:43:48 -0700

    add disclaimer to new files

--- src/pymgrid/Environments/pymgrid_csda.py
+++ src/pymgrid/Environments/pymgrid_csda.py
@@ -1,4 +1,24 @@
+"""
+Copyright 2020 Total S.A
+Authors:Gonzague Henri <gonzague.henri@total.com>
+Permission to use, modify, and distribute this software is given under the
+terms of the pymgrid License.
+NO WARRANTY IS EXPRESSED OR IMPLIED.  USE AT YOUR OWN RISK.
+$Date: 2020/10/21 07:43 $
+Gonzague Henri
+"""
 
+"""
+<pymgrid is a Python library to simulate microgrids>
+Copyright (C) <2020> <Total S.A.>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+"""
 from pymgrid.Environments.Environment import Environment
 import numpy as np
 
