6b1bb8b5e923194be6e2eebba5a7688524c8b67e
Gonzague Henri <gonzague.henri@total.com>
2020-11-12 11:29:00 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages("src"),
     version="0.1.0",
-    python_requires=">=3.7",
+    python_requires=">=3.0",
     include_package_data=True,
     install_requires=[
         "requests",
