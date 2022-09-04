354dd7c754d32c92a237ae351dbe4cc130099a95
avishaihalev <avishai.halev@external.total.com>
2022-09-04 17:38:20 +0100

    Revert "src/pymgrid/microgrid/modules/base/base_timeseries_module.py" This reverts commit 906e83ae583676c3b5859996dc3ec8f5e2e394a7.

--- src/pymgrid/microgrid/modules/base/base_module.py
+++ src/pymgrid/microgrid/modules/base/base_module.py
@@ -1,11 +1,11 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 import numpy as np
 from pymgrid.microgrid.utils.logger import ModularLogger
 from pymgrid.microgrid.utils.normalize import Normalize, IdentityNormalize
 from gym.spaces import Box
 
 
-class BaseMicrogridModule(ABC):
+class BaseMicrogridModule:
     """
     Base class for all microgrid _modules.
     All values passed to step(self) that result in non-negative
