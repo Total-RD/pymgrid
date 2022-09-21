e4d932fb7ee487de1970bc47b21a2fd2cc74b086
ahalev <avishaihalev@gmail.com>
2022-09-20 17:16:19 -0700

    add conversion tests

---
+++ tests/microgrid/modules/conversion_test/test_modular_conversion.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from tests.helpers.test_case import TestCase
+
+
+class TestToModular(TestCase):
+    def setUp(self) -> None:
+        from src.pymgrid.MicrogridGenerator import MicrogridGenerator
+        mgen = MicrogridGenerator()
+        mgen.generate_microgrid()
+        self.weak_grids = [microgrid for microgrid in mgen.microgrids if self.is_weak_grid(microgrid)]
+        self.genset_only = [microgrid for microgrid in mgen.microgrids if not microgrid.architecture["grid"]]
+        self.strong_grid_only = [microgrid for microgrid in mgen.microgrids if
+                                 (not microgrid.architecture["genset"]) and self.is_strong_grid(microgrid)]
+        self.strong_grid_and_genset = [microgrid for microgrid in mgen.microgrids if
+                                       microgrid.architecture["genset"] and self.is_strong_grid(microgrid)]
+
+
+    @staticmethod
+    def is_weak_grid(microgrid):
+        return microgrid.architecture["grid"] and microgrid._grid_status_ts.min().item() < 1
+
+    @staticmethod
+    def is_strong_grid(microgrid):
+        return microgrid.architecture["grid"] and microgrid._grid_status_ts.min().item() == 1
+
+    def test_weak_grid_conversion_failure(self):
+        for microgrid in self.weak_grids:
+                with self.assertRaisesRegex(ValueError, "Weak grids are currently not supported by ModularMicrogrid."):
+                    microgrid.to_modular()
+
+    def test_genset_only(self):
+        for microgrid in self.genset_only:
+            modular = microgrid.to_modular()
+            self.assertTrue(len(modular.genset) == 1)
+
+            genset_module = modular.genset[0]
+            self.assertEqual(microgrid.genset.fuel_cost, genset_module.fuel_cost_per_unit)
+            self.assertEqual(microgrid.genset.co2, genset_module.co2_per_unit)
+            self.assertEqual(microgrid.genset.rated_power*microgrid.genset.p_max, genset_module.max_production)
+
+            with self.assertRaises(AttributeError):
+                _ = modular.grid
