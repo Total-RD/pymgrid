7fba90884315bcd2f96015e920d13af749219c7a
ahalev <avishaihalev@gmail.com>
2022-09-28 16:10:16 -0700

    fix whitespace

--- tests/test_microgrid.py
+++ tests/test_microgrid.py
@@ -14,7 +14,6 @@ from pymgrid.MicrogridGenerator import MicrogridGenerator
 
 
 class TestMicrogrid(unittest.TestCase):
-
     def setUp(self):
         mgen = MicrogridGenerator()
         self.mg = mgen._create_microgrid()
@@ -32,7 +31,6 @@ class TestMicrogrid(unittest.TestCase):
         self.mg.set_horizon(25)
         self.assertEqual(25, self.mg.horizon)
 
-
     def test_get_updated_values(self):
         mg_data = self.mg.get_updated_values()
         self.assertEqual(0, mg_data['pv'])
@@ -40,44 +38,32 @@ class TestMicrogrid(unittest.TestCase):
     def test_forecast_all(self):
         self.mg.set_horizon(24)
         forecast = self.mg.forecast_all()
-
         self.assertEqual(24, len(forecast['load']))
-
 
     def test_forecast_pv(self):
         self.mg.set_horizon(24)
         forecast = self.mg.forecast_pv()
-
         self.assertEqual (24, len(forecast))
-
 
     def test_forecast_load(self):
         self.mg.set_horizon(24)
         forecast = self.mg.forecast_load()
-
         self.assertEqual (24, len(forecast))
-
 
     def test_run(self):
         pv1 = self.mg.forecast_pv()[1]
         self.mg.run(self.random_control())
-        pv2 =  self.mg.pv
-
+        pv2 = self.mg.pv
         self.assertEqual(pv1, pv2)
-
 
     def test_train_test_split(self):
         self.mg.train_test_split()
-
         self.assertEqual('training',self.mg._data_set_to_use)
 
     def test_reset(self):
         self.mg.run(self.random_control())
         self.mg.reset()
-
         self.assertEqual (0, self.mg._tracking_timestep)
-
-
 
 
 if __name__ == '__main__':
