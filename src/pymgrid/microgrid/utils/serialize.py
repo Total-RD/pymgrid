2fd38f8d88c0f1ccb34e570b4e69d41d2d267198
ahalev <avishaihalev@gmail.com>
2022-11-01 22:03:02 -0700

    add numpy constructors

--- src/pymgrid/microgrid/utils/serialize.py
+++ src/pymgrid/microgrid/utils/serialize.py
@@ -14,8 +14,15 @@ def _numpy_represent_int(dumper, data):
     return dumper.represent_int(data.item())
 
 
+def _numpy_constructor_seq(loader, node):
+    return np.array(loader.construct_sequence(node))
+
+
 def add_numpy_representers():
     yaml.SafeDumper.add_representer(np.ndarray, _numpy_representer_seq)
     yaml.SafeDumper.add_multi_representer(np.floating, _numpy_represent_floating)
     yaml.SafeDumper.add_multi_representer(np.integer, _numpy_represent_int)
 
+
+def add_numpy_constructors():
+    yaml.SafeLoader.add_constructor('!ndarray:', _numpy_constructor_seq)
