36a68b04bc93b3defaebc9020a936f5b1c9d0949
ahalev <avishaihalev@gmail.com>
2022-11-03 23:52:06 -0700

    add dump_csv

--- src/pymgrid/microgrid/utils/serialize.py
+++ src/pymgrid/microgrid/utils/serialize.py
@@ -2,6 +2,35 @@ import numpy as np
 import pandas as pd
 import yaml
 
+from pathlib import Path
+
+TO_CSV_TYPES = np.ndarray, pd.core.generic.NDFrame
+
+
+def _dump_data(data, path):
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(data).to_csv(path)
+    return str(path)
+
+
+def dump_csvs(data_dict, stream, yaml_tag):
+    if not hasattr(stream, "name"):
+        return data_dict
+
+    path = Path(stream.name).parent / "data"
+
+    for data_type, data in data_dict.items():
+        if isinstance(data, dict):
+            for key, val in data.items():
+                if isinstance(val, TO_CSV_TYPES):
+                    data[key] = _dump_data(val, path / f"{data_type}/{yaml_tag.lstrip('!')}_{key}.csv")
+
+        elif isinstance(data, TO_CSV_TYPES):
+            data_dict[data_type] = _dump_data(data, path / f"{yaml_tag.strip('!')}_{data_type}.csv")
+
+    return data_dict
+
 
 def _numpy_representer_seq(dumper, data):
     return dumper.represent_sequence('!ndarray:', data.tolist())
