614aa7f502d1e6929e1d5e7cc0f2f8c21395d539
ahalev <avishaihalev@gmail.com>
2022-11-03 23:51:09 -0700

    add pandas representers

--- src/pymgrid/microgrid/utils/serialize.py
+++ src/pymgrid/microgrid/utils/serialize.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import yaml
 
 
@@ -14,15 +15,25 @@ def _numpy_represent_int(dumper, data):
     return dumper.represent_int(data.item())
 
 
+def _pandas_representer_df(dumper, data):
+    return dumper.represent_mapping('!DataFrame', data.to_dict())
+
+
 def _numpy_constructor_seq(loader, node):
     return np.array(loader.construct_sequence(node))
 
 
-def add_numpy_representers():
+def _pandas_constructor_df(loader, node):
+    return pd.DataFrame(loader.construct_mapping(node))
+
+
+def add_numpy_pandas_representers():
     yaml.SafeDumper.add_representer(np.ndarray, _numpy_representer_seq)
+    yaml.SafeDumper.add_representer(pd.DataFrame, _pandas_representer_df)
     yaml.SafeDumper.add_multi_representer(np.floating, _numpy_represent_floating)
     yaml.SafeDumper.add_multi_representer(np.integer, _numpy_represent_int)
 
 
-def add_numpy_constructors():
-    yaml.SafeLoader.add_constructor('!ndarray:', _numpy_constructor_seq)
+def add_numpy_pandas_constructors():
+    yaml.SafeLoader.add_constructor('!ndarray', _numpy_constructor_seq)
+    yaml.SafeLoader.add_constructor('!DataFrame', _pandas_constructor_df)
