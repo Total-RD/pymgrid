aac25a372a5e2e8992fb96d8b0bc2686ea9745f9
ahalev <avishaihalev@gmail.com>
2022-11-08 23:39:21 -0800

    add ndarray subclass and np constructors

--- src/pymgrid/microgrid/utils/serialize.py
+++ src/pymgrid/microgrid/utils/serialize.py
@@ -28,8 +28,16 @@ def add_path_to_arr_like(data_dict, path, yaml_tag):
     return data_dict
 
 
-def _numpy_representer_seq(dumper, data):
-    return dumper.represent_sequence('!ndarray', data.tolist())
+def add_numpy_pandas_representers():
+    yaml.SafeDumper.add_representer(pd.DataFrame, _pandas_df_representer)
+    yaml.SafeDumper.add_multi_representer(np.ndarray, _numpy_arr_representer)
+    yaml.SafeDumper.add_multi_representer(np.floating, _numpy_represent_floating)
+    yaml.SafeDumper.add_multi_representer(np.integer, _numpy_represent_int)
+
+
+def add_numpy_pandas_constructors():
+    yaml.SafeLoader.add_constructor('!NDArray', _numpy_arr_constructor)
+    yaml.SafeLoader.add_constructor('!DataFrame', _pandas_df_constructor)
 
 
 def _numpy_represent_floating(dumper, data):
@@ -40,25 +48,67 @@ def _numpy_represent_int(dumper, data):
     return dumper.represent_int(data.item())
 
 
-def _pandas_representer_df(dumper, data):
-    return dumper.represent_mapping('!DataFrame', data.to_dict())
+def _numpy_arr_representer(dumper, data):
+    return _arr_representer(dumper, data, 'NDArray')
 
 
-def _numpy_constructor_seq(loader, node):
-    return np.array(loader.construct_sequence(node))
+def _pandas_df_representer(dumper, data):
+    return _arr_representer(dumper, data, 'DataFrame')
 
 
-def _pandas_constructor_df(loader, node):
-    return pd.DataFrame(loader.construct_mapping(node))
+def _arr_representer(dumper, data, r_type):
+    if hasattr(data, "path"):
+        rel_path = _dump_representation(data, data.path, Path(dumper.stream.name).parent)
+        return dumper.represent_scalar(f'!{r_type}', rel_path)
+
+    try:
+        return dumper.represent_mapping(f'!{r_type}', data.to_dict())
+    except AttributeError:
+        return dumper.represent_sequence(f'!{r_type}', data.tolist())
 
 
-def add_numpy_pandas_representers():
-    yaml.SafeDumper.add_representer(np.ndarray, _numpy_representer_seq)
-    yaml.SafeDumper.add_representer(pd.DataFrame, _pandas_representer_df)
-    yaml.SafeDumper.add_multi_representer(np.floating, _numpy_represent_floating)
-    yaml.SafeDumper.add_multi_representer(np.integer, _numpy_represent_int)
+def _dump_representation(data, path, stream_loc):
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+    pd.DataFrame(data).to_csv(path)
+    return str(path.relative_to(stream_loc))
 
 
-def add_numpy_pandas_constructors():
-    yaml.SafeLoader.add_constructor('!ndarray', _numpy_constructor_seq)
-    yaml.SafeLoader.add_constructor('!DataFrame', _pandas_constructor_df)
+def _pandas_df_constructor(loader, node):
+    if isinstance(node, yaml.MappingNode):
+        return pd.DataFrame(loader.construct_mapping(node))
+
+    data_path = Path(loader.construct_scalar(node))
+
+    if not data_path.is_absolute():
+        try:
+            stream_name = loader.stream.name
+        except AttributeError:
+            raise ValueError(f"Path must be absolute if yaml stream has no 'name'.")
+
+        data_path = Path(stream_name).parent / data_path
+
+    return pd.read_csv(data_path, index_col=0)
+
+
+def _numpy_arr_constructor(loader, node):
+    if isinstance(node, yaml.SequenceNode):
+        return np.array(loader.construct_sequence(node))
+
+    return _pandas_df_constructor(loader, node).values
+
+
+class NDArraySubclass(np.ndarray):
+    """
+    A simple python class that allows a 'path' attribute for serialization.
+    `path` may be lost if object is manipulated.
+    """
+    def __new__(cls, input_array, path=None):
+        obj = np.asarray(input_array).view(cls)
+        obj.path = path
+        return obj
+
+    def __array_finalize__(self, obj):
+        if obj is None:
+            return
+        self.path = getattr(obj, 'path', None)
