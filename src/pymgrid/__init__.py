bdb5456116801436950c4e4d430566c6aea15186
ahalev <avishaihalev@gmail.com>
2022-11-22 15:40:50 -0500

    refactor deprecated imports

--- src/pymgrid/__init__.py
+++ src/pymgrid/__init__.py
@@ -4,6 +4,7 @@ __version__ = (Path(__file__).parent.parent.parent / "version.txt").read_text()
 
 PROJECT_PATH = Path(__file__).parent
 
-from pymgrid._deprecated.non_modular_microgrid import NonModularMicrogrid
+from ._deprecated.non_modular_microgrid import NonModularMicrogrid
 from .modular_microgrid import Microgrid
 from .MicrogridGenerator import MicrogridGenerator
+
