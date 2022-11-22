b06461d69bb425553c891060203b39ccf22665ca
ahalev <avishaihalev@gmail.com>
2022-11-22 15:39:09 -0500

    refactor env test imports

--- tests/envs/discrete.py
+++ tests/envs/discrete.py
@@ -1,7 +1,7 @@
 from tests.helpers.test_case import TestCase
 from tests.helpers.modular_microgrid import get_modular_microgrid
 
-from pymgrid.microgrid.envs import DiscreteMicrogridEnv
+from pymgrid.envs import DiscreteMicrogridEnv
 
 
 class TestDiscreteEnv(TestCase):
