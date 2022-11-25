9bba42100f1f795aa4726cd5b136b7bbe3e3bde4
ahalev <avishaihalev@gmail.com>
2022-11-25 14:47:57 -0500

    add error again

--- src/pymgrid/__init__.py
+++ src/pymgrid/__init__.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 PROJECT_PATH = Path(__file__).parent
-
-__version__ = (PROJECT_PATH / "version.txt").read_text()
+try:
+    __version__ = (PROJECT_PATH / "version.txt").read_text()
+except FileNotFoundError:
+    raise FileNotFoundError(PROJECT_PATH / "version.txt")
 
 from ._deprecated.non_modular_microgrid import NonModularMicrogrid
 from .microgrid import Microgrid
