fe3bf922f4f0389a39fd0bf14fb9bce55b8bca12
ahalev <avishaihalev@gmail.com>
2022-11-25 14:44:39 -0500

    move version.txt to within pymgrid

--- src/pymgrid/__init__.py
+++ src/pymgrid/__init__.py
@@ -1,10 +1,8 @@
 from pathlib import Path
-try:
-    __version__ = (Path(__file__).parent.parent.parent / "version.txt").read_text()
-except FileNotFoundError:
-    raise FileNotFoundError(f'Failed with __init__ path: {Path(__file__)}')
 
 PROJECT_PATH = Path(__file__).parent
+
+__version__ = (PROJECT_PATH / "version.txt").read_text()
 
 from ._deprecated.non_modular_microgrid import NonModularMicrogrid
 from .microgrid import Microgrid
