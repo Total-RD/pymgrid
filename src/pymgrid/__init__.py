d45430afc15eb5c13daece51a4d49a948e6c61f9
ahalev <avishaihalev@gmail.com>
2022-11-25 15:09:22 -0500

    better fnf error

--- src/pymgrid/__init__.py
+++ src/pymgrid/__init__.py
@@ -4,7 +4,11 @@ PROJECT_PATH = Path(__file__).parent
 try:
     __version__ = (PROJECT_PATH / "version.txt").read_text()
 except FileNotFoundError:
-    raise FileNotFoundError(PROJECT_PATH / "version.txt")
+    file = PROJECT_PATH / "version.txt"
+    nl ='\n'
+    err_msg = f'version.txt file not found at {file}.\n' \
+              f'Contents of parent dir:\n{nl.join(list(sorted(file.parent.iterdir())))}'
+    raise FileNotFoundError(err_msg)
 
 from ._deprecated.non_modular_microgrid import NonModularMicrogrid
 from .microgrid import Microgrid
