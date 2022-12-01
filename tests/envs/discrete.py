fbcb86d4db4c0add9dbfa7c07edaa0156551b271
ahalev <avishaihalev@gmail.com>
2022-12-01 13:50:46 -0800

    remove flat_modules attr

--- tests/envs/discrete.py
+++ tests/envs/discrete.py
@@ -5,8 +5,25 @@ from pymgrid.envs import DiscreteMicrogridEnv
 
 
 class TestDiscreteEnv(TestCase):
-    def test_init(self):
+    def test_init_from_microgrid(self):
         microgrid = get_modular_microgrid()
-        env_2 = DiscreteMicrogridEnv(microgrid)
+        env = DiscreteMicrogridEnv(microgrid)
 
-        print("Here")
+        self.assertEqual(env.modules, microgrid.modules)
+        self.assertIsNot(env.modules.module_tuples(), microgrid.modules.module_tuples())
+
+        n_obs = sum([x.observation_spaces['normalized'].shape[0] for x in microgrid.module_list])
+
+        self.assertEqual(env.observation_space.shape, (n_obs,))
+
+    def test_init_from_modules(self):
+        microgrid = get_modular_microgrid()
+        env = DiscreteMicrogridEnv(microgrid.modules.module_tuples(), add_unbalanced_module=False)
+
+        self.assertEqual(env.modules, microgrid.modules)
+        self.assertIsNot(env.modules.module_tuples(), microgrid.modules.module_tuples())
+
+        n_obs = sum([x.observation_spaces['normalized'].shape[0] for x in microgrid.module_list])
+
+        self.assertEqual(env.observation_space.shape, (n_obs,))
+
