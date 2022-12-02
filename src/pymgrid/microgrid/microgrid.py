245d78306e309229d32ce046f81d6a1669b0e51b
ahalev <avishaihalev@gmail.com>
2022-12-01 17:47:56 -0800

    update sample_action and get_empty_action to handle null action spaces

--- src/pymgrid/microgrid/microgrid.py
+++ src/pymgrid/microgrid/microgrid.py
@@ -275,7 +275,9 @@ class Microgrid(yaml.YAMLObject):
         """
 
         module_iterator = self._modules.module_dict() if sample_flex_modules else self._modules.fixed.module_dict()
-        return {module_name: [module.sample_action(strict_bound=strict_bound) for module in module_list] for module_name, module_list in module_iterator.items()}
+        return {module_name: [module.sample_action(strict_bound=strict_bound) for module in module_list]
+                for module_name, module_list in module_iterator.items()
+                if module_list[0].action_space.shape[0]}
 
     def get_empty_action(self, sample_flex_modules=False):
         """
@@ -298,7 +300,8 @@ class Microgrid(yaml.YAMLObject):
         """
         module_iterator = self._modules.module_dict() if sample_flex_modules else self._modules.fixed.module_dict()
 
-        return {module_name: [None]*len(module_list) for module_name, module_list in module_iterator.items()}
+        return {module_name: [None]*len(module_list) for module_name, module_list in module_iterator.items()
+                if module_list[0].action_space.shape[0]}
 
     def to_normalized(self, data_dict, act=False, obs=False):
         """
