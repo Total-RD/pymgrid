15aa70a8997acfd8604ae9680f417fa9710b9ffe
ahalev <avishaihalev@gmail.com>
2022-12-02 16:39:18 -0800

    add some additional unittests

--- tests/microgrid/test_microgrid.py
+++ tests/microgrid/test_microgrid.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from pymgrid import Microgrid
+from pymgrid.modules import LoadModule, RenewableModule
 
 from tests.helpers.modular_microgrid import get_modular_microgrid
 from tests.helpers.test_case import TestCase
@@ -65,3 +66,69 @@ class TestMicrogrid(TestCase):
                         has_corresponding_action = False
 
                     self.assertTrue(empty_action_space != has_corresponding_action)  # XOR
+
+
+class TestMicrogridLoadPV(TestCase):
+    def setUp(self):
+        self.load_ts, self.pv_ts = self.set_ts()
+        self.microgrid = self.set_microgrid()
+
+    def set_ts(self):
+        ts = 10 * np.random.rand(100)
+        return ts, ts
+
+    def set_microgrid(self):
+        load = LoadModule(time_series=self.load_ts, raise_errors=True)
+        pv = RenewableModule(time_series=self.pv_ts, raise_errors=True)
+        return Microgrid([load, pv])
+
+    def test_populated_correctly(self):
+        self.assertTrue(hasattr(self.microgrid.modules, 'load'))
+        self.assertTrue(hasattr(self.microgrid.modules, 'renewable'))
+        self.assertEqual(len(self.microgrid.modules), 3)  # load, pv, unbalanced
+
+    def test_current_load_correct(self):
+        self.assertEqual(self.microgrid.modules.load[0].current_load, self.load_ts[0])
+
+    def test_current_pv_correct(self):
+        self.assertEqual(self.microgrid.modules.renewable[0].current_renewable, self.pv_ts[0])
+
+    def test_run_one_step(self):
+        pass
+
+    def test_run_n_steps(self):
+        for step in range(len(self.load_ts)):
+            with self.subTest(step=step):
+                pass
+
+
+class TestMicrogridLoadExcessPV(TestMicrogridLoadPV):
+    #  Same as above but pv is greater than load.
+    def set_ts(self):
+        load_ts = 10*np.random.rand(100)
+        pv_ts = load_ts + 5*np.random.rand(100)
+        return load_ts, pv_ts
+
+    def test_run_one_step(self):
+        pass
+
+    def test_run_n_steps(self):
+        for step in range(len(self.load_ts)):
+            with self.subTest(step=step):
+                pass
+
+
+class TestMicrogridPVExcessLoad(TestMicrogridLoadPV):
+    # Load greater than PV.
+    def set_ts(self):
+        pv_ts = 10 * np.random.rand(100)
+        load_ts = pv_ts + 5 * np.random.rand(100)
+        return load_ts, pv_ts
+
+    def test_run_one_step(self):
+        pass
+
+    def test_run_n_steps(self):
+        for step in range(len(self.load_ts)):
+            with self.subTest(step=step):
+                pass
