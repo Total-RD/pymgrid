bd1dfbc7aedfe602ceef3c4763842cfa94f4600f
ahalev <avishaihalev@gmail.com>
2022-12-04 17:46:13 -0800

    add type check when falling through to numpy assertions

--- tests/helpers/test_case.py
+++ tests/helpers/test_case.py
@@ -10,5 +10,8 @@ class TestCase(unittest.TestCase):
         except (ValueError, AssertionError):
             try:
                 np.testing.assert_equal(first, second, err_msg=msg if msg else '')
-            except AssertionError:
-                np.testing.assert_allclose(first, second, rtol=1e-7, atol=1e-10, err_msg=msg if msg else '')
+            except AssertionError as e:
+                try:
+                    np.testing.assert_allclose(first, second, rtol=1e-7, atol=1e-10, err_msg=msg if msg else '')
+                except TypeError:
+                    raise e
