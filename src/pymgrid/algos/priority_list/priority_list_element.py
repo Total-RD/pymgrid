86aa5961c65883a1123da1bd51e367d172c02b1c
ahalev <avishaihalev@gmail.com>
2022-12-14 16:48:00 -0800

    add marginal_cost and ordering to PriorityListElement

--- src/pymgrid/algos/priority_list/priority_list_element.py
+++ src/pymgrid/algos/priority_list/priority_list_element.py
@@ -1,9 +1,32 @@
 from dataclasses import dataclass
-from typing import Tuple
+from functools import total_ordering
+from typing import Tuple, Optional
 
 
-@dataclass(frozen=True)
+@total_ordering
+@dataclass(frozen=True, eq=False)
 class PriorityListElement:
     module: Tuple[str, int]
     module_actions: int
     action: int
+    marginal_cost: Optional[float] = None
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+
+        return (
+                self.module == other.module and
+                self.module_actions == other.module_actions and
+                self.action == other.action and
+                self.marginal_cost == other.marginal_cost
+        )
+
+    def __lt__(self, other):
+        if type(self) != type(other) or self.marginal_cost is None or other.marginal_cost is None:
+            return NotImplemented
+
+        return (
+                self.marginal_cost < other.marginal_cost or
+                (self.marginal_cost == other.marginal_cost and self.action > other.action)
+        )
