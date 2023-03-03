525a8fb17d79ac6134dc9041a0fe3338e7d3b5df
ahalev <avishaihalev@gmail.com>
2023-03-02 17:50:19 -0800

    allow return of modules only and custom timeseries length

--- tests/helpers/modular_microgrid.py
+++ tests/helpers/modular_microgrid.py
@@ -11,7 +11,12 @@ from pymgrid.modules import (
 )
 
 
-def get_modular_microgrid(remove_modules=(), retain_only=None, additional_modules=None, add_unbalanced_module=True):
+def get_modular_microgrid(remove_modules=(),
+                          retain_only=None,
+                          additional_modules=None,
+                          add_unbalanced_module=True,
+                          timeseries_length=100,
+                          modules_only=False):
 
     modules = dict(
         genset=GensetModule(running_min_production=10, running_max_production=50, genset_cost=0.5),
@@ -23,11 +28,11 @@ def get_modular_microgrid(remove_modules=(), retain_only=None, additional_module
                               efficiency=1.0,
                               init_soc=0.5),
 
-        renewable=RenewableModule(time_series=50*np.ones(100)),
+        renewable=RenewableModule(time_series=50*np.ones(timeseries_length)),
 
-        load=LoadModule(time_series=60*np.ones(100)),
+        load=LoadModule(time_series=60*np.ones(timeseries_length)),
 
-        grid=GridModule(max_import=100, max_export=0, time_series=np.ones((100, 3)), raise_errors=True)
+        grid=GridModule(max_import=100, max_export=0, time_series=np.ones((timeseries_length, 3)), raise_errors=True)
         )
 
     if retain_only is not None:
@@ -43,5 +48,8 @@ def get_modular_microgrid(remove_modules=(), retain_only=None, additional_module
 
     modules = list(modules.values())
     modules.extend(additional_modules if additional_modules else [])
+
+    if modules_only:
+        return modules
 
     return Microgrid(modules, add_unbalanced_module=add_unbalanced_module)
