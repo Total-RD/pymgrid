f8899ab2a296c1d667c7d54ec2fe3be3c6fa969b
ahalev <avishaihalev@gmail.com>
2023-03-29 22:02:08 -0700

    add dry_run to init files

--- src/pymgrid/__init__.py
+++ src/pymgrid/__init__.py
@@ -7,11 +7,13 @@ from ._deprecated.non_modular_microgrid import NonModularMicrogrid
 from .microgrid import Microgrid
 from .MicrogridGenerator import MicrogridGenerator
 
-from .utils import add_pymgrid_yaml_representers
+from .utils import add_pymgrid_yaml_representers, dry_run
 
 import pymgrid.envs
 
 __all__ = [
+    'add_pymgrid_yaml_representers',
+    'dry_run',
     'Microgrid',
     'MicrogridGenerator',
     'NonModularMicrogrid',
